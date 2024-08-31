b02496d8858c54029ef3bf2d3e558d451b72201f
SimranjeetKaurTussar <simranjeetkaurtussar@gmail.com>
2024-08-31 23:41:23 +0530

    done with utils

--- myutils/src/App.js
+++ myutils/src/App.js
@@ -5,11 +5,7 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
  
 function App() {
@@ -40,24 +36,19 @@ function App() {
   }
   return (
     <>
-    <Router>
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    <Switch>
-    {/* /users --> Component 1
-        /users/home --> Component 2 */}
-          <Route exact path="/about">
-            <About mode={mode} />
-          </Route>
-          <Route exact path="/">
-            <TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-          </Route>
-    </Switch>
-    </div>
-    </Router>
-    </> 
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} key={new Date()} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes> {/* Replace Switch with Routes */}
+            <Route exact path="/about" element={<About mode={mode} />} /> {/* Use element prop */}
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
+
 
 export default App;
